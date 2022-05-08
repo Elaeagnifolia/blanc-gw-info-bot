9e82fdbb25d38927a310493043eed5a32357fe48
Elaeagnifolia <j.wong205914@gmail.com>
2022-05-08 01:53:53 -0500

    Update blanc_gw_bot.py

--- blanc_gw_bot.py
+++ blanc_gw_bot.py
@@ -2,6 +2,7 @@
 # Imports
 import discord
 import os
+import cachetools.func
 
 from apiclient import discovery
 from google.oauth2 import service_account
@@ -16,7 +17,6 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 SPREADSHEET_ID = os.getenv('SPREADSHEET_ID')
 RANGE_NAME = os.getenv('SPREADSHEET_RANGE')
 AUTH = os.path.join(os.getcwd(), 'blanc-gw-sheet-credentials.json')
-
 BOT_COMMAND_CHANNEL = {
     "test": int(os.getenv('BOT_COMMAND_TEST')),
     "prod": int(os.getenv('BOT_COMMAND_PROD'))
@@ -25,13 +25,13 @@ BOT_COMMAND_CHANNEL = {
 # Constants
 PREFIX = '?'
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
-GW_DATA = []
+SPREADSHEET_CACHE_DURATION = 21600
 
 client = discord.Client()
 
 ### Class for organizing spreadsheet data
 class BlancGwInfo:
-    def __init__(self, id, name, main_element, activity_check, checks, strikes, pass_used):
+    def __init__(self, id, name, main_element, activity_check, checks, strikes, pass_used, pass_element):
         self.id = id
         self.name = name
         self.main_element = main_element
@@ -39,17 +39,32 @@ class BlancGwInfo:
         self.checks = checks
         self.strikes = strikes
         self.pass_used = pass_used
+        self.pass_element = pass_element
 
     def get_total(self):
         return int(self.activity_check) + int(self.checks) + int(self.strikes)
 
 ### Util Methods
+@cachetools.func.ttl_cache(ttl=SPREADSHEET_CACHE_DURATION)
+def get_spreadsheet_data():
+    print("[INFO] Retrieving data from Google Sheets...")
+    # Log in to Google service account
+    credentials = service_account.Credentials.from_service_account_file(AUTH, scopes=SCOPES)
+    service = discovery.build('sheets', 'v4', credentials=credentials)
+
+    # Access the Google Sheets API with the service account
+    sheet = service.spreadsheets()
+
+    # Retrieve the values in the guild spreadsheet in the given range
+    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME).execute()
+    return result.get('values', [])
+
 def load_data_from_spreadsheet(values):
     gw_data = []
     for row in values:
         # Skip if no name, placeholder name (e.g. -), or if it's the header row
         if ((row[1] != '' and row[1] != '-') and row[0] != '2022 Pass used?'):
-            gw_data.append(BlancGwInfo(row[8], row[1], row[2], row[3], row[4], row[5], row[0] != "NO"))
+            gw_data.append(BlancGwInfo(row[8], row[1], row[2], row[3], row[4], row[5], row[0] != "NO", row[6]))
     return gw_data
 
 def does_message_need_processing(message, env):
@@ -63,9 +78,9 @@ def does_message_need_processing(message, env):
 
     return True
 
-def get_gw_data(discord_id):
+def find_matching_accounts(gw_data, discord_id):
     matches = []
-    for data in GW_DATA:
+    for data in gw_data:
         if str(data.id) == str(discord_id):
             matches.append(data)
     return matches
@@ -74,10 +89,14 @@ def build_gw_info_dm(member_data):
     message = f'{len(member_data)} account(s) was/were found linked to your Discord username.\n'
     message = message + '------------------------------------\n'
     for data in member_data:
+        pass_used_msg = str(data.pass_used)
+        if data.pass_used:
+            pass_used_msg = pass_used_msg + ' (' + data.pass_element + ')'
+
         total = data.get_total()
         message = message + f'__**{data.name}**__\n'
         message = message + f'**Main Element**: {data.main_element}\n'
-        message = message + f'**Pass Used?**: {data.pass_used}\n'
+        message = message + f'**Pass Used?**: {pass_used_msg}\n'
         message = message + f'**Activity Check**: {data.activity_check}\n'
         message = message + f'**Checks**: {data.checks}\n'
         message = message + f'**Strikes**: {data.strikes}\n'
@@ -113,11 +132,25 @@ async def on_message(message):
     print('=============================================')
 
     # Check if message matches command
-    if message.content.startswith(PREFIX + 'gwinfo'):      
+    if message.content.startswith(PREFIX + 'gwinfo'):  
+        message_split = message.content.split()
+
         print(f'[INFO] ?gwinfo invoked by {message.author.name}.')
         print(f'[INFO] Looking up GW info data for {message.author.name}.')
 
-        member_data = get_gw_data(message.author.id)
+        # If the -force flag was specified, clear cache to force getting new values from Google
+        if '-force' in message_split:
+            print('[INFO] Clearing cache to force retrieval of new Google Sheets info...')
+            get_spreadsheet_data.cache_clear()
+
+        spreadsheet_data = get_spreadsheet_data()
+        
+        if len(spreadsheet_data) <= 0:
+            print('No data found.')
+            return
+
+        gw_data = load_data_from_spreadsheet(spreadsheet_data)            
+        member_data = find_matching_accounts(gw_data, message.author.id)
 
         if len(member_data) == 0:
             print(f'[ERROR] GW data could not be found for {message.author.name} ({messsage.author.id}).')
@@ -135,23 +168,6 @@ async def on_message(message):
 
 # Try logging in to Google Sheets and get values from spreadsheet and starting Discord when data is loaded
 try:
-    # Log in to Google service account
-    credentials = service_account.Credentials.from_service_account_file(AUTH, scopes=SCOPES)
-    service = discovery.build('sheets', 'v4', credentials=credentials)
-
-    # Access the Google Sheets API with the service account
-    sheet = service.spreadsheets()
-
-    # Retrieve the values in the guild spreadsheet in the given range
-    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME).execute()
-    values = result.get('values', [])
-
-    if not values:
-        print('No data found.')
-    else:
-        GW_DATA = load_data_from_spreadsheet(values)
-
-        # Start Discord client
-        client.run(TOKEN)
+    client.run(TOKEN)
 except OSError as err:
     print(err)
